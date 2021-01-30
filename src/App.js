483ab7e15a72204098bed4a58c7f0a1a87f357e1
byjfernandes <joyce_f.s@hotmail.com>
2021-01-29 22:18:47 -0300

    adicionando o component MovieRow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Tmdb from './Tmdb';
+import { useEffect, useState } from 'react';
+import MovieRow from './components/MovieRow';
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+useEffect(()=>{
+  const loadAll = async () => {
+    let list = await Tmdb.getHomeList();
+    setMovieList(list);
+  }
+
+  loadAll();
+}, []);
+
+
   return (
-    <div>
-      Olá mundo!
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <div>
+            <MovieRow key={key} title={item.title} items={item.items}/>
+          </div>
+        ))}
+      </section>
     </div>
   )
 }
